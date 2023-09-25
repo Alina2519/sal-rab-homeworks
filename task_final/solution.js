12bcaccaeaaa75133a5c8650d807ee1416ce1c8b
Alina <aitpaeva0912@mail.ru>
2023-09-25 11:28:56 +0300

    cheange task_Final

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,12 +42,12 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул." + address.street + ", дом " + adress.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = `${name}` + " " + `${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
